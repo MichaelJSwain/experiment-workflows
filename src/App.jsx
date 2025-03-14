552934aafbba9fbdee62a222c73e34b0120f0dcf
MichaelSwain <michaeljswain@gmail.com>
2025-03-14 15:12:01 +0100

    update ui when status is changes

--- src/App.jsx
+++ src/App.jsx
@@ -544,323 +544,13 @@ function App() {
   const [selectedProject, setSelectedProject] = useState("th");
   
   function fetchExperiments() {
-    const experiments = [
-      {
-        "allocation_policy": "manual",
-        "audience_conditions": "everyone",
-        "campaign_id": 14190210469,
-        "changes": [],
-        "created": "2019-03-21T09:18:53.313626Z",
-        "description": "",
-        "holdback": 0,
-        "id": 14221830212,
-        "is_classic": false,
-        "last_modified": "2019-03-21T09:21:49.329152Z",
-        "metrics": [],
-        "name": "Test 1",
-        "page_ids": [
-          14208030178
-        ],
-        "project_id": 14193350179,
-        "status": "archived",
-        "traffic_allocation": 10000,
-        "type": "a/b",
-        "variations": [
-          {
-            "actions": [
-              {
-                "changes": [],
-                "page_id": 14208030178,
-                "share_link": "https://se.devtestp1.tommy.com/womens-hoodies-sweatshirts?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=14190260193&optimizely_x_audiences=&optimizely_preview_layer_ids=14190210469&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "name": "Original",
-            "status": "active",
-            "variation_id": 14190260193,
-            "weight": 5000
-          },
-          {
-            "actions": [
-              {
-                "changes": [
-                  {
-                    "async": false,
-                    "dependencies": [],
-                    "id": "F41E23FD-74E6-4179-9EC8-138675BCEA65",
-                    "type": "custom_code",
-                    "value": "alert('Test URL change');"
-                  }
-                ],
-                "page_id": 14208030178,
-                "share_link": "https://se.devtestp1.tommy.com/womens-hoodies-sweatshirts?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=14208140009&optimizely_x_audiences=&optimizely_preview_layer_ids=14190210469&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "name": "Variation #1",
-            "status": "active",
-            "variation_id": 14208140009,
-            "weight": 5000
-          }
-        ]
-      },
-      {
-        "allocation_policy": "manual",
-        "audience_conditions": "everyone",
-        "campaign_id": 14268130320,
-        "changes": [],
-        "created": "2019-03-28T08:51:13.523971Z",
-        "description": "",
-        "holdback": 0,
-        "id": 14277660358,
-        "is_classic": false,
-        "last_modified": "2019-03-28T08:51:13.524001Z",
-        "metrics": [],
-        "name": "Homepage example",
-        "page_ids": [
-          14285590348
-        ],
-        "project_id": 14193350179,
-        "status": "archived",
-        "traffic_allocation": 10000,
-        "type": "a/b",
-        "variations": [
-          {
-            "actions": [
-              {
-                "changes": [],
-                "page_id": 14285590348,
-                "share_link": "http://htttps://uk.tommy.com?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=14268080210&optimizely_x_audiences=&optimizely_preview_layer_ids=14268130320&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "name": "Original",
-            "status": "active",
-            "variation_id": 14268080210,
-            "weight": 5000
-          },
-          {
-            "actions": [
-              {
-                "changes": [],
-                "page_id": 14285590348,
-                "share_link": "http://htttps://uk.tommy.com?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=14291520514&optimizely_x_audiences=&optimizely_preview_layer_ids=14268130320&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "name": "Variation #1",
-            "status": "active",
-            "variation_id": 14291520514,
-            "weight": 5000
-          }
-        ]
-      },
-      {
-        "allocation_policy": "manual",
-        "audience_conditions": "everyone",
-        "campaign_id": 14549290297,
-        "changes": [],
-        "created": "2019-04-18T07:58:41.679740Z",
-        "description": "",
-        "holdback": 0,
-        "id": 14528870833,
-        "is_classic": false,
-        "last_modified": "2024-11-14T14:00:06.492748Z",
-        "metrics": [],
-        "name": "Example 2",
-        "page_ids": [
-          6456458930290688
-        ],
-        "project_id": 14193350179,
-        "status": "archived",
-        "traffic_allocation": 10000,
-        "type": "a/b",
-        "variations": [
-          {
-            "actions": [],
-            "archived": false,
-            "name": "Original",
-            "status": "active",
-            "variation_id": 14523091046,
-            "weight": 5000
-          },
-          {
-            "actions": [],
-            "archived": false,
-            "name": "Variation #1",
-            "status": "active",
-            "variation_id": 14563230608,
-            "weight": 5000
-          }
-        ]
-      },
-      {
-        "allocation_policy": "manual",
-        "audience_conditions": "everyone",
-        "campaign_id": 5838890649518080,
-        "changes": [
-          {
-            "async": false,
-            "dependencies": [],
-            "id": "4a64afa8-db0b-4279-9f3d-89c913575580",
-            "type": "custom_code",
-            "value": "console.log('test')"
-          },
-          {
-            "dependencies": [],
-            "id": "011e1402-333f-4cbb-a9cf-b083f974b225",
-            "selector": "head",
-            "type": "custom_css",
-            "value": ".selector {background: 'red'}"
-          }
-        ],
-        "created": "2024-11-21T12:37:06.352410Z",
-        "description": "description placeholder",
-        "holdback": 0,
-        "id": 4508690657574912,
-        "is_classic": false,
-        "last_modified": "2024-11-21T12:37:06.352428Z",
-        "metrics": [
-          {
-            "aggregator": "sum",
-            "field": "revenue",
-            "scope": "visitor",
-            "winning_direction": "increasing"
-          }
-        ],
-        "name": "CX123 - red background",
-        "page_ids": [
-          6677488684761088
-        ],
-        "project_id": 14193350179,
-        "status": "archived",
-        "traffic_allocation": 10000,
-        "type": "a/b",
-        "variations": [
-          {
-            "actions": [
-              {
-                "changes": [
-                  {
-                    "async": false,
-                    "dependencies": [],
-                    "id": "02e81c35-1238-48f5-bd17-e08d6b71803f",
-                    "type": "custom_code",
-                    "value": "console.log('test')"
-                  },
-                  {
-                    "dependencies": [],
-                    "id": "f2e02f93-cf8b-4314-b187-9f119e5d5735",
-                    "selector": "head",
-                    "type": "custom_css",
-                    "value": ".selector {background: 'red'}"
-                  }
-                ],
-                "page_id": 6677488684761088,
-                "share_link": "https://uk.tommy.com/women?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=5354395387297792&optimizely_x_audiences=&optimizely_preview_layer_ids=5838890649518080&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "description": "variant description",
-            "name": "variant0",
-            "status": "active",
-            "variation_id": 5354395387297792,
-            "weight": 5000
-          },
-          {
-            "actions": [
-              {
-                "changes": [
-                  {
-                    "async": false,
-                    "dependencies": [],
-                    "id": "bc3ab871-4716-445c-895a-24356daa067e",
-                    "type": "custom_code",
-                    "value": "console.log('test')"
-                  },
-                  {
-                    "dependencies": [],
-                    "id": "ea5e3880-5f90-4000-ad0b-4a72dd775b6c",
-                    "selector": "head",
-                    "type": "custom_css",
-                    "value": ".selector {background: 'red'}"
-                  }
-                ],
-                "page_id": 6677488684761088,
-                "share_link": "https://uk.tommy.com/women?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=5768090965770240&optimizely_x_audiences=&optimizely_preview_layer_ids=5838890649518080&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "description": "variant description",
-            "name": "variant1",
-            "status": "active",
-            "variation_id": 5768090965770240,
-            "weight": 5000
-          }
-        ]
-      },
-      {
-        "allocation_policy": "manual",
-        "audience_conditions": "[\"and\", {\"audience_id\": 6161659085979648}]",
-        "campaign_id": 6245000074231808,
-        "changes": [],
-        "created": "2024-11-29T13:27:26.177876Z",
-        "description": "description placeholder",
-        "holdback": 0,
-        "id": 4593679906635776,
-        "is_classic": false,
-        "last_modified": "2024-11-29T13:27:26.177890Z",
-        "metrics": [
-          {
-            "aggregator": "sum",
-            "field": "revenue",
-            "scope": "visitor",
-            "winning_direction": "increasing"
-          }
-        ],
-        "name": "[QA] - CX678 - button test",
-        "page_ids": [
-          5524941324419072
-        ],
-        "project_id": 14193350179,
-        "status": "archived",
-        "traffic_allocation": 10000,
-        "type": "a/b",
-        "variations": [
-          {
-            "actions": [
-              {
-                "changes": [],
-                "page_id": 5524941324419072,
-                "share_link": "https://uk.tommy.com/women?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=6127605598912512&optimizely_x_audiences=6161659085979648&optimizely_preview_layer_ids=6245000074231808&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "description": "variant description",
-            "name": "variant0",
-            "status": "active",
-            "variation_id": 6127605598912512,
-            "weight": 5000
-          },
-          {
-            "actions": [
-              {
-                "changes": [],
-                "page_id": 5524941324419072,
-                "share_link": "https://uk.tommy.com/women?optimizely_token=c91a5a15fd4b686e7cd20371cf3d642e638a6cb88b05c758f3742bada872366b&optimizely_x=6303115142496256&optimizely_x_audiences=6161659085979648&optimizely_preview_layer_ids=6245000074231808&optimizely_snippet=s3-14193350179"
-              }
-            ],
-            "archived": false,
-            "description": "variant description",
-            "name": "variant1",
-            "status": "active",
-            "variation_id": 6303115142496256,
-            "weight": 5000
-          }
-        ]
-      }
-    ];
-    return experiments;
+    let response;
+    if (selectedProject === "th") {
+      response = experiments_th;
+    } else {
+      response = experiments_ck;
+    }
+    return response;
     // const options = {
     //   method: 'GET',
     //   headers: {
@@ -880,17 +570,32 @@ function App() {
     setSelectedProject(project);
   };
 
+  function handleExperimentStateChange(experiment, state) {
+    console.log(experiment, state);
+
+    /* call api 
+     ....
+     */
+
+    // update UI
+    const copy = experiments.map(exp => {
+      const copyExp = {...exp};
+      if (exp.id === experiment.id) {
+        copyExp.status = state;
+      }
+      return copyExp
+    });
+    setExperiments(copy);
+  }
+
   useEffect(() => {
     const fetchedExperiments = fetchExperiments()
-    setExperiments(experiments_th);
+    setExperiments(fetchedExperiments);
   }, []);
 
   useEffect(() => {
-    if (selectedProject === "th") {
-      setExperiments(experiments_th);
-    } else {
-      setExperiments(experiments_ck);
-    }
+    const fetchedExperiments = fetchExperiments();
+    setExperiments(fetchedExperiments);
   }, [selectedProject]);
 
   return (
@@ -916,13 +621,14 @@ function App() {
                       <h3>Type</h3>
                     </div>
                     <div className='table-data'>
-                      <h3>Status</h3>
+                      <h3>{exp.status}</h3>
                     </div>
                     <div className='table-data'>
-                      <select>
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
+                      <select onChange={(e) => {handleExperimentStateChange(exp, e.target.value)}}>
+                        <option>Run</option>
+                        <option>Pause</option>
+                        <option>Archive</option>
+                        <option>Conclude</option>
                       </select>
                     </div>
                     
